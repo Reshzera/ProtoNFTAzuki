3274aac44ba3b6c491da8bac11813fa60c3d2635
Reshzera <reoshiro13@gmail.com>
2024-03-25 19:24:11 -0300

    feat: first commit

--- contracts/ProtoNTF.sol
+++ contracts/ProtoNTF.sol
@@ -1,61 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "erc721a/contracts/ERC721A.sol";
 
-contract ProtoNTF is
-    ERC721,
-    ERC721Enumerable,
-    ERC721Burnable,
-    ERC721URIStorage
-{
-    uint256 private _tokenIdCounter;
+contract ProtoNTF is ERC721A {
+    address payable private _owner;
 
-    constructor() ERC721("ProtoNTF", "PNTF") {}
+    constructor() ERC721A("ProtoNTF", "PNTF") {
+        _owner = payable(msg.sender);
+    }
+
+    function mint(uint256 quantity) public payable {
+        require(msg.value == quantity * 0.01 ether, "Invalid amount");
+        _mint(msg.sender, quantity);
+    }
+
+    function burn(uint256 tokenId) public {
+        super._burn(tokenId);
+    }
+
+    function withdraw() public {
+        require(msg.sender == _owner, "Only owner can withdraw");
+        uint256 balance = address(this).balance;
+        (bool success, ) = _owner.call{value: balance}("");
+        require(success, "Transfer failed.");
+    }
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://gateway.pinata.cloud/ipfs/QmZ/";
-    }
-
-    function mint() public {
-        _tokenIdCounter++;
-        _safeMint(msg.sender, _tokenIdCounter);
-        _setTokenURI(_tokenIdCounter, Strings.toString(_tokenIdCounter));
-    }
-
-    function _increaseBalance(
-        address account,
-        uint128 value
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
+        return "ipfs://QmRLwwfSyPYga8BvamLM1sT2W8R9JxLDZQWJy6ZqRh5RUB/";
     }
 
     function tokenURI(
         uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    ) public view override(ERC721A) returns (string memory) {
         return string.concat(super.tokenURI(tokenId), ".json");
-    }
-
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
-    ) internal override(ERC721, ERC721Enumerable) returns (address) {
-        return super._update(to, tokenId, auth);
-    }
-
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 }
--- test/ProtoNTF.ts
+++ test/ProtoNTF.ts
@@ -1,5 +1,6 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
+import { parseEther } from "ethers";
 import hre from "hardhat";
 
 describe("ProtoNTF", function () {
@@ -26,10 +27,10 @@ describe("ProtoNTF", function () {
   it("Should mint a token", async function () {
     const { protoNTF, owner } = await loadFixture(deployFixture);
 
-    await protoNTF.mint();
+    await protoNTF.mint(1, { value: parseEther("0.01") });
 
     const ownerBalance = await protoNTF.balanceOf(owner.address);
-    const ownerOfToken = await protoNTF.ownerOf(1);
+    const ownerOfToken = await protoNTF.ownerOf(0);
     const totalSupply = await protoNTF.totalSupply();
 
     expect(ownerBalance).to.equal(1);
@@ -37,19 +38,79 @@ describe("ProtoNTF", function () {
     expect(totalSupply).to.equal(1);
   });
 
+  it("Should not mint a token if not enough value", async function () {
+    const { protoNTF } = await loadFixture(deployFixture);
+
+    await expect(
+      protoNTF.mint(1, { value: parseEther("0.009") })
+    ).to.be.revertedWith("Invalid amount");
+  });
+
+  it("Should mint multiple tokens", async function () {
+    const { protoNTF, owner } = await loadFixture(deployFixture);
+    await protoNTF.mint(3, { value: parseEther("0.03") });
+
+    const ownerBalance = await protoNTF.balanceOf(owner.address);
+    const totalSupply = await protoNTF.totalSupply();
+
+    expect(ownerBalance).to.equal(3);
+    expect(totalSupply).to.equal(3);
+  });
+
   it("Should has URI metadata", async function () {
     const { protoNTF } = await loadFixture(deployFixture);
 
-    await protoNTF.mint();
+    await protoNTF.mint(1, { value: parseEther("0.01") });
 
-    const tokenURI = await protoNTF.tokenURI(1);
+    const tokenURI = await protoNTF.tokenURI(0);
 
-    expect(tokenURI).to.equal("https://gateway.pinata.cloud/ipfs/QmZ/1.json");
+    expect(tokenURI).to.equal(
+      "ipfs://QmRLwwfSyPYga8BvamLM1sT2W8R9JxLDZQWJy6ZqRh5RUB/0.json"
+    );
   });
 
-  it("Should support interface", async function () {
-    const { protoNTF } = await loadFixture(deployFixture);
+  it("Should burn a token", async function () {
+    const { protoNTF, owner } = await loadFixture(deployFixture);
 
-    expect(await protoNTF.supportsInterface("0x80ac58cd")).to.equal(true);
+    await protoNTF.mint(1, { value: parseEther("0.01") });
+    await protoNTF.burn(0);
+
+    const ownerBalance = await protoNTF.balanceOf(owner.address);
+    const totalSupply = await protoNTF.totalSupply();
+
+    expect(ownerBalance).to.equal(0);
+    expect(totalSupply).to.equal(0);
+  });
+
+  it("Should withdraw contract balance", async function () {
+    const { protoNTF, owner, otherAccount } = await loadFixture(deployFixture);
+    const otherAccoutInstance = protoNTF.connect(otherAccount);
+    await otherAccoutInstance.mint(1, { value: parseEther("0.01") });
+
+    const ownerBalanceBefore = await hre.ethers.provider.getBalance(
+      owner.address
+    );
+
+    await protoNTF.withdraw();
+
+    const contractBalance = await hre.ethers.provider.getBalance(
+      await protoNTF.getAddress()
+    );
+    const ownerBalance = await hre.ethers.provider.getBalance(owner.address);
+
+    expect(contractBalance).to.equal(0);
+    expect(ownerBalance - ownerBalanceBefore).to.lessThanOrEqual(
+      parseEther("0.01")
+    );
+  });
+
+  it("Should not with draw contract balance if not owner", async function () {
+    const { protoNTF, owner, otherAccount } = await loadFixture(deployFixture);
+    const otherAccoutInstance = protoNTF.connect(otherAccount);
+    await otherAccoutInstance.mint(1, { value: parseEther("0.01") });
+
+    await expect(otherAccoutInstance.withdraw()).to.be.revertedWith(
+      "Only owner can withdraw"
+    );
   });
 });
